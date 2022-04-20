beb5d795acacf232b969111978dcd071ba8f6ee5
MC <81518660+MatteoChendi@users.noreply.github.com>
2022-04-21 01:05:10 +0200

    Update index.js

--- 1. Sun Time Project/index.js
+++ 1. Sun Time Project/index.js
@@ -8,11 +8,16 @@
 
 const searchInput = document.getElementById("search-input") 
 const searchBtn = document.getElementById("search-btn")
+const saveBtn = document.getElementById("save-btn")
 const mapCity = document.getElementById("mappa")
 const sunrise = document.getElementById("sunrise")
 const sunset = document.getElementById("sunset")
+let savedItems = document.getElementById("saved-items")
 const errorMessage = document.getElementById("error-message")
-const mapGoogle = document.getElementById("map-google")
+const deleteAllBtn = document.getElementById("delete-all")
+// let's define a variable that contains the values in localStorage, so they will be rendered out when page is refreshed
+const itemsFromLocalStorage = JSON.parse(localStorage.getItem("emptyListSavedItems"))
+
 let cityName = ""
 // access key is taken from "OpenWeather API"
 const accessKey = "7abd9140ffaee6ecffd6022f8f27c79e"
@@ -98,7 +103,6 @@ document.addEventListener("keyup", function(event) {
 })
 // check whether the search bar is empty. If empty do not trigger the search function
     
-
 // 2. connect location searched with sunrise/sunset time and display them with the location on a new "box" below (centered and well designed)
 // // 2.1 when clicked, display a centered box below with features below
 // // 2.2 create: Location name, Sunrise time, Sunset time and snapshot of location on world map
@@ -106,7 +110,67 @@ document.addEventListener("keyup", function(event) {
 // OK // 2.3.1 adjust overall size of div, so that it not go outside the webpage and proportion are maintained equal (both hight and width)
 
 // 3. create button to save that search and add it below permanently
-// // 3.1 save button to save last search
+// OK // 3.1 save button to save last search
+let emptyListSavedItems = []
+
+// let's create a separate function to render the saved items below
+function render(listObject) {
+    let listItems = ""
+    // iterate throught empty list length. Every time clicked on SAVE, add item on list 
+    // display every saved item as a <div> below, with city name & country code
+    // if item already in list, do not push it again and display error message
+   
+    // in case it is the first search, always add location to the list
+    for (i=0; i<listObject.length; i++) {  
+        add = `<div>${listObject[i]}</div>`
+        listItems += add
+        }
+    savedItems.innerHTML = listItems
+    console.log(savedItems.innerHTML)
+}
+
+// // check if the location you want to save is already saved
+// // BONUS: capitalize the search so "milano" == "Milano" // //
+
+saveBtn.addEventListener("click", function() {
+    // first "if" is to avoid saving "LOCATION" in saved items list
+    if (mapCity.innerHTML == "LOCATION") {
+    } else {
+        if (emptyListSavedItems.length > 0) {
+            for (i=0; i<emptyListSavedItems.length; i++) {
+                if (emptyListSavedItems.includes(mapCity.innerHTML)) {
+                    errorMessage.innerHTML = "Sorry, it looks like this location is already saved!"
+                    break
+                } else {
+                    emptyListSavedItems.push(mapCity.innerHTML)
+                    break
+                }
+            }       
+        } else {
+            emptyListSavedItems.push(mapCity.innerHTML)
+        }
+    }
+    
+    render(emptyListSavedItems)
+    
+    localStorage.setItem("emptyListSavedItems", JSON.stringify(emptyListSavedItems))
+    let retrieveLocalStorage = localStorage.getItem("emptyListSavedItems")
+    console.log(`This is the local storage: ${JSON.parse(retrieveLocalStorage)}`)
+})
+
+// setting localStorage to true, we will render items saved in local storage when page is refreshed
+if (itemsFromLocalStorage) {
+    emptyListSavedItems = itemsFromLocalStorage
+    render(itemsFromLocalStorage)
+}
+
+// implement a DELETE ALL button to clear local storage
+deleteAllBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    emptyListSavedItems = []
+    render(emptyListSavedItems)
+})
+
 // // 3.2 saved location will be displayed on a line (bar) below
 // // 3.3 add possibility to click on previous saved location to change main box displayed ("VIEW" button near each saved location)
 // // 3.4 if changed, last location will be lost if not saved. If saved will be simply added below (and in localStorage)
