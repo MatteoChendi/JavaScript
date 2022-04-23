72dfb4d97ccf4b3cbcdd80b2d39455f5f4852015
MC <81518660+MatteoChendi@users.noreply.github.com>
2022-04-23 11:44:16 +0200

    Update index.js

--- 1. Sun Time Project/index.js
+++ 1. Sun Time Project/index.js
@@ -1,10 +1,9 @@
-// Create a widget that allows you to search by location (cities) and display hours of sunrise and sunset
+// Create an extension that allows you to search by location (cities) and display hours of sunrise and sunset
  
 // OK 1. create search bar and connect it with API locations -- time of sunrise/sunset has to be updated by syncronizing to underlying database
 // OK // 1.1 create the design for the page
 // OK // 1.2 create search bar
 // OK // create a Sarch button, so when typed you can confirm
-
 
 const searchInput = document.getElementById("search-input") 
 const searchBtn = document.getElementById("search-btn")
@@ -19,11 +18,9 @@ const errorMessage = document.getElementById("error-message")
 const deleteAllBtn = document.getElementById("delete-all")
 // let's define a variable that contains the values in localStorage, so they will be rendered out when page is refreshed
 const itemsFromLocalStorage = JSON.parse(localStorage.getItem("emptyListSavedItems"))
-
 let cityName = ""
 // access key is taken from "OpenWeather API"
 const accessKey = "7abd9140ffaee6ecffd6022f8f27c79e"
-//const accessKeyGoogleMaps = ""
 
 // OK // the Search button triggers a function(): get a JSON request for the typed location 
 async function getData() {
@@ -101,12 +98,12 @@ function format_time(s) {
   
 
 // OK // // find a free API with all the main locations in the world --> search by location name 
-// // // get the location when typing in the search bar
-// // // create an updatable list with first 3 location similar by name when typing
+// TODO ADVANCED // // get the location when typing in the search bar
+// TODO ADVANCED // // create an updatable list with first 3 location similar by name when typing
 
 
-// // 1.3 display provisional results based on input
-// // 1.4 add the possibility to either click on the result 
+// TODO ADVANCED // 1.3 display provisional results based on input
+// OK // 1.4 add the possibility to either click on the result 
 // OK // (OPTIONAL: or press "enter" when selected instead of clicking on the button)
 document.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
@@ -114,15 +111,14 @@ document.addEventListener("keyup", function(event) {
         searchBtn.click()
         };  
 })
-// check whether the search bar is empty. If empty do not trigger the search function
+// OK check whether the search bar is empty. If empty do not trigger the search function
     
 // 2. connect location searched with sunrise/sunset time and display them with the location on a new "box" below (centered and well designed)
 // OK // 2.1 when clicked, display a centered box below with features below
-// // 2.2 create: Location name, Sunrise time, Sunset time and snapshot of location on world map
-// OK // 2.3 import sunset and sunrise emoji and display them in proper boxes on the side
-// OK // 2.3.1 adjust overall size of div, so that it not go outside the webpage and proportion are maintained equal (both hight and width)
+// OK // 2.2 import sunset and sunrise emoji and display them in proper boxes on the side
+// OK // 2.3 adjust overall size of div, so that it not go outside the webpage and proportion are maintained equal (both hight and width)
 
-// 3. create button to save that search and add it below permanently
+// OK // 3. create button to save that search and add it below permanently
 // OK // 3.1 save button to save last search
 let emptyListSavedItems = []
 
@@ -147,7 +143,7 @@ function render(listObject) {
 }
 
 // OK // check if the location you want to save is already saved
-// // BONUS: capitalize the search so "milano" == "Milano" // //
+// OK // BONUS: capitalize the search so "city" == "City"
 
 saveBtn.addEventListener("click", function() {
     // first "if" is to avoid saving "LOCATION" in saved items list
@@ -198,9 +194,8 @@ deleteAllBtn.addEventListener("dblclick", function() {
     deleteBtn.style.display = 'none';
 })
 
-// // 3.2 saved location will be displayed on a line (bar) below
+// OK // 3.2 saved location will be displayed on a line (bar) below
 // // 3.3 add possibility to click on previous saved location to change main box displayed ("VIEW" button near each saved location)
-// // 3.4 if changed, last location will be lost if not saved. If saved will be simply added below (and in localStorage)
+// OK // 3.4 if changed, last location will be lost if not saved. If saved will be simply added below (and in localStorage)
 // // 3.3 create button DELETE if the user wants to delete what previously saved near each saved location ("DELETE" button near each saved location)
 
-// 4. When new search and select it, substitute new search with wath displayed in box
